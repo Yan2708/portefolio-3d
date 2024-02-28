b23fa5cc6111f9a073f5b9aa073c1575dd4d8c7b
Yan2708 <74718439+Yan2708@users.noreply.github.com>
2024-02-28 19:56:40 +0100

    add plane

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -1,81 +1,67 @@
 <script lang="ts">
-  import { T } from '@threlte/core'
-  import { ContactShadows, Float, Grid, OrbitControls } from '@threlte/extras'
+	import { T, useTask, useThrelte } from '@threlte/core';
+	import { Float, Grid, OrbitControls } from '@threlte/extras';
+	import Paperplane from './models/paperplane.svelte';
+	import { Raycaster, Vector2, Vector3 } from 'three';
+	import { onMount } from 'svelte';
+	import { interactivity } from '@threlte/extras'
+
+  let intersectionPoint = new Vector3()
+  let cRotation
+  
+  interactivity()
+
+  const { camera, scene } = useThrelte()
+
+	onMount(() => {
+
+    const raycaster = new Raycaster()
+    const pointer = new Vector2()
+
+		function onPointerMove(event: { clientX: number; clientY: number }) {
+			pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+			pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
+      raycaster.setFromCamera(pointer, $camera)
+      const intersects = raycaster.intersectObjects(scene.children)
+      intersectionPoint = intersects[0]?.point ?? new Vector3()
+      
+
+		}
+
+		window.addEventListener('pointermove', onPointerMove);
+		return () => {
+			window.removeEventListener('pointermove', onPointerMove);
+		};
+    
+	});
+  
+  useTask((delta) => {
+    cRotation=camera.current.rotation.toArray()
+  })
 </script>
 
-<T.PerspectiveCamera
-  makeDefault
-  position={[-10, 10, 10]}
-  fov={15}
->
-  <OrbitControls
-    autoRotate
-    enableZoom={false}
-    enableDamping
-    autoRotateSpeed={0.5}
-    target.y={1.5}
-  />
+<T.PerspectiveCamera makeDefault position={[-10, 10, 10]} fov={50}>
+	<OrbitControls enableDamping target.y={1.5} />
 </T.PerspectiveCamera>
 
-<T.DirectionalLight
-  intensity={0.8}
-  position.x={5}
-  position.y={10}
-/>
+<T.DirectionalLight intensity={2} position.x={5} position.y={10} />
 <T.AmbientLight intensity={0.2} />
 
 <Grid
-  position.y={-0.001}
-  cellColor="#ffffff"
-  sectionColor="#ffffff"
-  sectionThickness={0}
-  fadeDistance={25}
-  cellSize={2}
+	position.y={-0.001}
+	cellColor="#ffffff"
+	sectionColor="#ffffff"
+	sectionThickness={0}
+	fadeDistance={100}
+	cellSize={2}
 />
 
-<ContactShadows
-  scale={10}
-  blur={2}
-  far={2.5}
-  opacity={0.5}
-/>
-
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position.y={1.2}
-    position.z={-0.75}
-  >
-    <T.BoxGeometry />
-    <T.MeshStandardMaterial color="#0059BA" />
-  </T.Mesh>
+<Float floatingRange={[1, 2]}>
+	<Paperplane  rotation={[intersectionPoint.y, 0, intersectionPoint.y, 'ZXY']}
+  />
 </Float>
 
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position={[1.2, 1.5, 0.75]}
-    rotation.x={5}
-    rotation.y={71}
-  >
-    <T.TorusKnotGeometry args={[0.5, 0.15, 100, 12, 2, 3]} />
-    <T.MeshStandardMaterial color="#F85122" />
-  </T.Mesh>
-</Float>
-
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position={[-1.4, 1.5, 0.75]}
-    rotation={[-5, 128, 10]}
-  >
-    <T.IcosahedronGeometry />
-    <T.MeshStandardMaterial color="#F8EBCE" />
-  </T.Mesh>
-</Float>
+<T.Mesh rotation={cRotation}>
+	<T.PlaneGeometry args={[20, 20,20]} />
+	<T.MeshStandardMaterial color="hotpink"/>
+</T.Mesh>
